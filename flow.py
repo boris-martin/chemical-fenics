9acfa5b7f0319e1c82f15b48848ee88501e79d7d
Boris Martin <boris.martin.be@gmail.com>
2022-05-01 15:44:16 +0200

    added Neumann BC example

--- flow.py
+++ flow.py
@@ -1,9 +1,8 @@
 from fenics import *
-from matplotlib import interactive
-from sympy import degree
 
 
 mesh = UnitSquareMesh(8, 8)
+normal = FacetNormal(mesh)
 
 # Manufactured solution : 1 + x² + 2y² => -laplacian = f is -6
 V = FunctionSpace(mesh, 'P', 1)
@@ -11,7 +10,7 @@ V = FunctionSpace(mesh, 'P', 1)
 u_D = Expression('x[0]*x[0] + 2*x[1]*x[1] + 1', degree=2)
 
 def boundary(x, on_boundary):
-    return on_boundary
+    return on_boundary and (not near(x[0], 1) or near(x[1], 0) or near(x[1], 1))
 
 bc = DirichletBC(V, u_D, boundary)
 
@@ -19,8 +18,9 @@ bc = DirichletBC(V, u_D, boundary)
 u = TrialFunction(V)
 v = TestFunction(V)
 f = Constant(-6.0)
+g = Constant(2.0)
 a = dot(grad(u), grad(v)) * dx
-L = f * v * dx
+L = f * v * dx + g * v * ds
 
 u = Function(V)
 solve(a == L, u, bc)
