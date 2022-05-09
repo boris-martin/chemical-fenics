d6aee4325b4fc8ba1788f862b08f232feb5987f7
Boris Martin <boris.martin.be@gmail.com>
2022-05-09 15:49:50 +0200

    draft preCICE

--- chemistry-precice.py
+++ chemistry-precice.py
@@ -1,9 +1,11 @@
 from fenics import *
 from mshr import *
+import fenicsprecice
+import numpy as np
 
 T = 10.0           # final time
 num_steps = 100    # number of time steps
-dt = T / num_steps # time step size
+default_dt = T / num_steps # time step size
 
 domain = Rectangle(Point(0,0), Point(4, 1))
 mesh = generate_mesh(domain, 32)
@@ -12,10 +14,22 @@ normal = FacetNormal(mesh)
 # Three dimensional vector for three species
 P = FiniteElement('P', triangle, 1)
 V = FunctionSpace(mesh, MixedElement([P, P, P]))
+W = FunctionSpace(mesh, MixedElement([P, P])) # Velocity field is 2D
 
 # No BCs: only homgenuous Von Neumann. It means nothing leaves the domain
 # Von neumann BCs give rise to surface loads, which are simply zero here.
 
+class CouplingDomain(SubDomain):
+    def inside(self, x, on_boundary):
+        return True
+
+# Initialize preCICE
+precice = fenicsprecice.Adapter(adapter_config_filename="chemistry-config.json")
+precice_dt = precice.initialize(coupling_subdomain=CouplingDomain(), read_function_space=W)
+
+flow = precice.create_coupling_expression()
+
+dt = np.min([default_dt, precice_dt])
 
 # Create used terms (_n suffix means "at previous step")
 u_ = Function(V)
@@ -28,11 +42,12 @@ v_1, v_2, v_3 = split(v)
 
 f = Expression(('x[0]/4', '1 - x[0]/4'), degree=1)
 k = Constant(1. / dt)
+
 r = Constant(1.0) # Reaction speed
 diff = Constant(0.01) # Diffusivity
 
 # Velocity field (from preCICE in the future)
-flow = Expression(("vmax * 4*x[1]*(1-x[1])", "0"), degree=2, vmax=0.5)
+# flow = Expression(("vmax * 4*x[1]*(1-x[1])", "0"), degree=2, vmax=0.5)
 
 u_n1, u_n2, u_n3 = split(u_n)
 F = k * (u_1 - u_n1) * v_1 * dx + 0.5 * dot(flow, grad(u_1 + u_n1)) * v_1 * dx + diff * dot(grad(u_1), grad(v_1)) * dx - dot(f[0], v_1) * dx + r * u_1 * u_2 * v_1 * dx + \
@@ -42,11 +57,20 @@ F = k * (u_1 - u_n1) * v_1 * dx + 0.5 * dot(flow, grad(u_1 + u_n1)) * v_1 * dx +
 
 
 t = 0
-vtkfileA = File('out/chemical_A.pvd')
-vtkfileB = File('out/chemical_B.pvd')
-vtkfileC = File('out/chemical_C.pvd')
+vtkfileA = File('out/chemical_A_precice.pvd')
+vtkfileB = File('out/chemical_B_precice.pvd')
+vtkfileC = File('out/chemical_C_precice.pvd')
 
-for n in range(num_steps):
+
+# No implicit coupling
+while precice.is_coupling_ongoing():
+
+    read_data = precice.read_data()
+    precice.update_coupling_expression(flow, read_data)
+    # If we add writing, do it here
+
+    dt = np.min([default_dt, precice_dt])
+    k.assign(1. / dt)
 
     t += dt 
     print("Time {:.3g} of {:.3g}".format(t, T))
@@ -61,3 +85,5 @@ for n in range(num_steps):
     vtkfileA << u_A, t
     vtkfileB << u_B, t
     vtkfileC << u_C, t
+
+    precice_dt = precice.advance(dt(0))
